35a03eadfcc375f99e30838ccfd3ed38cf1fecd4
rachmatjackly <rachmatjackly@streamzinu.com>
2022-04-22 21:31:02 +0700

    Fix port for heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
 const fs = require('fs');
+const port = process.env.PORT || 8000;
 const { phoneNumberFormatter } = require('./helpers/formatter');
 
 const app = express();
@@ -147,6 +148,6 @@ app.post('/send-media', (req, res) => {
     });
 });
 
-server.listen(8000, function () {
-    console.log('App running on :' + 8000)
+server.listen(port, function () {
+    console.log('App running on :' + port)
 })
